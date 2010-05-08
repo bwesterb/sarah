1407b146fb5a05f36340010900f69742cd2bf38b
Bas Westerbaan <bas@westerbaan.name>
2010-05-08 23:05:33 +0200

    threading: add KeyboardInterruptableEvent
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

---
+++ threading.py
@@ -0,0 +1,22 @@
+import select
+import socket
+
+class KeyboardInterruptableEvent(object):
+	""" Acts like threading.Event, but a call to .wait will interrupt on
+	    a keyboard interrupt """
+	def __init__(self):
+		self.flag = False
+		self.sp = socket.socketpair()
+	def wait(self, timeout=None):
+		select.select((self.sp[1],), (), (), timeout)
+	def set(self):
+		self.sp[0].send('Good morning!')
+	def isSet(self):
+		rs, ws, xs = select.select((self.sp[1],), (), (), 0)
+		return not self.sp[1] in rs
+	def clear(self):
+		while True:
+			read = self.sp[0].recv(4096)
+			if read != 4096:
+				break
+
