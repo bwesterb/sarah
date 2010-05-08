c819cc34cac0c1037038e499e84b94e2b1e5bbea
Bas Westerbaan <bas@westerbaan.name>
2010-05-08 19:35:56 +0200

    Add event
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

---
+++ event.py
@@ -0,0 +1,8 @@
+class Event(object):
+	def __init__(self):
+		self.handlers = []
+	def register(self, handler):
+		self.handlers.append(handler)
+	def __call__(self, *args, **kwargs):
+		for handler in self.handlers:
+			handler(*args, **kwargs)
