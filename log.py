3aee4a005adb5e8ba1877d75a419bc3fb6bffcac
Bas Westerbaan <bas@westerbaan.name>
2010-05-08 23:52:56 +0200

    log: reimplement using UnixSocketServer
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- log.py
+++ log.py
@@ -1,8 +1,4 @@
-from __future__ import with_statement
-
 import os
-import socket
-import select
 import logging
 import threading
 import logging.handlers
@@ -10,8 +6,9 @@ import logging.handlers
 from mirte.core import Module
 from sarah.io import IntSocketFile
 from sarah.runtime import ExceptionCatchingWrapper
+from sarah.socketserver import UnixSocketServer
 
-class LogServer(Module):
+class LogServer(UnixSocketServer):
 	class SocketHandler(logging.handlers.SocketHandler):
 		def __init__(self, socket):
 			self.__socket = socket
@@ -29,7 +26,6 @@ class LogServer(Module):
 					self._on_exception)
 			self.handler = LogServer.SocketHandler(self.f)
 			self.socket = _socket
-			self._sleep_socket_pair = socket.socketpair()
 			self.running = True
 
 		def _on_exception(self, attr, exc):
@@ -41,71 +37,18 @@ class LogServer(Module):
 		def interrupt(self):
 			self.f.interrupt()
 			self.running = False
-			self._sleep_socket_pair[0].send('good morning!')
 
 		def handle(self):
 			logging.getLogger('').addHandler(self.handler)
 			while self.server.running and self.running:
-				rlist, wlist, xlist = select.select((
-					self._sleep_socket_pair[1],), (),
-					(self.socket,))
-				if self.socket in xlist:
-					self.l.error("select(2) says socket "+
-							"in error")
+				rs, ws, xs = self.server.selectPool.select(
+						(), (), (self.socket,))
+				if self.socket in xs:
 					break
-				if self._sleep_socket_pair[1] in rlist:
-					self._sleep_socket_pair[1].recv(4096)
+
+		def cleanup(self):
 			logging.getLogger('').removeHandler(self.handler)
+			self.socket.close()
 
-	def __init__(self, settings, logger):
-		super(LogServer, self).__init__(settings, logger)
-		self.running = False
-		self._sleep_socket_pair = socket.socketpair()
-		self.n_conn = 0
-		self.handlers = set()
-		self.lock = threading.Lock()
-	def run(self):
-		assert not self.running
-		self.running = True
-		if os.path.exists(self.socketPath):
-			os.unlink(self.socketPath)
-		s = self.socket = socket.socket(socket.AF_UNIX,
-						socket.SOCK_STREAM)
-		s.bind(self.socketPath)
-		s.listen(3)
-		while self.running:
-			rlist, wlist, xlist = select.select([s,
-				self._sleep_socket_pair[1]], [], [s])
-			if (self._sleep_socket_pair[1] in rlist and
-			    not self.running):
-				break
-			if s in xlist:
-				self.l.error("select(2) says socket in error")
-				break
-			if not s in rlist:
-				continue
-			con, addr = s.accept()
-			self.n_conn += 1
-			self.threadPool.execute(self._handle_request,
-						con, addr, self.n_conn)
-	def _handle_request(self, con, addr, n_conn):
-		l = logging.getLogger("%s.%s" % (self.l.name, n_conn))
-		l.info('Accepted connection from %s' % addr)
-		handler = LogServer.Handler(self, con, l)
-		with self.lock:
-			self.handlers.add(handler)
-		try:
-			handler.handle()
-		except IOError:
-			l.warn("Handler caught IOError")
-		finally:
-			with self.lock:
-				self.handlers.remove(handler)
-
-	def stop(self):
-		self.running = False
-		self._sleep_socket_pair[0].send('good morning!')
-		with self.lock:
-			handlers = set(self.handlers)
-		for handler in handlers:
-			handler.interrupt()
+	def create_handler(self, con, addr, logger):
+		return LogServer.Handler(self, con, logger)
