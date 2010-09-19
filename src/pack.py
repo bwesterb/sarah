2445db42aab548a15efd6449c900e86e62bb7c52
Bas Westerbaan <bas@westerbaan.name>
2010-09-19 17:10:03 +0200

    pack: add {un,}pack_int
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- src/pack.py
+++ src/pack.py
@@ -1,3 +1,28 @@
+def unpack_int(s):
+	""" Reads a packed integer from string <s> """
+	ret = 0
+	i = 0
+	while True:
+		b = ord(s[i])
+		ret |= (b & 127) << (i*7)
+		i += 1
+		if b & 128 == 0:
+			break
+	return ret
+
+def pack_int(v):
+	""" Returns <v> as packed string. """
+	if v == 0:
+		return "\0"
+	ret = ''
+	while v > 0:
+		c = v & 127
+		v >>= 7
+		if v != 0:
+			c = c | 128
+		ret += chr(c)
+	return ret
+
 def read_packed_int(f):
 	""" Reads a packed integer from the fileobj <f>. """
 	ret = 0
