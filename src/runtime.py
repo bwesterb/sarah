cb6c79bb4222273be59acdd89b63df7649f99e51
Bas Westerbaan <bas@westerbaan.name>
2010-10-31 15:52:39 +0100

    runtime: add CallCatchingWrapper
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- src/runtime.py
+++ src/runtime.py
@@ -29,6 +29,35 @@ class ExceptionCatchingWrapper(object):
 		wrapped = object.__getattribute__(self, '_wrapped')
 		delattr(wrapped, name)
 
+class CallCatchingWrapper(object):
+	def __init__(self, wrapped, condition, callback):
+		""" Creates a wrapper around <wrapped>.  Calls to callable
+		    attributes which match <condition> are replaced by calls
+		    to <callback>(name, func, args, kwargs), where <name> is
+		    the name of the intercepted callable attribute, <func> the
+		    original function, <(kw)args> the (keyword) arguments which
+		    were passed to the call. The return value of the callback,
+		    is returned to the caller. """
+		object.__setattr__(self, '_wrapped', wrapped)
+		object.__setattr__(self, '_condition', condition)
+		object.__setattr__(self, '_callback', callback)
+	def __getattribute__(self, name):
+		wrapped = object.__getattribute__(self, '_wrapped')
+		condition = object.__getattribute__(self, '_condition')
+		a = getattr(wrapped, name)
+		if callable(a) and condition(name):
+			cb = object.__getattribute__(self, '_callback')
+			def wrapper(*args, **kwargs):
+				return cb(name, a, args, kwargs)
+			return wrapper
+		return a
+	def __setattr__(self, name, value):
+		wrapped = object.__getattribute__(self, '_wrapped')
+		setattr(wrapped, name, value)
+	def __delattr__(self, name):
+		wrapped = object.__getattribute__(self, '_wrapped')
+		delattr(wrapped, name)
+
 def _get_by_path(bits, _globals):
 	c = None
 	for i, bit in enumerate(bits):
