10a71d1cdd43a03ee2440d4191bfa76240928da2
Bas Westerbaan <bas@westerbaan.name>
2011-05-05 19:16:30 +0200

    _itertools: add iter_by_n
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- src/_itertools.py
+++ src/_itertools.py
@@ -2,3 +2,16 @@
 def pick(iterable):
 	return iter(iterable).next()
 
+def iter_by_n(iterable, n):
+        ret = []
+        it = iter(iterable)
+        while True:
+                while len(ret) < n:
+                        try:
+                                ret.append(next(it))
+                        except StopIteration:
+                                if ret:
+                                        yield ret
+                                return
+                yield ret
+                ret = []
